268afbaeb0967ef449f18f66db6354c679790abd
sogaci2 <113293174+sogaci2@users.noreply.github.com>
2022-09-12 10:00:53 +0200

    Update streamlif_app_sg.py

--- streamlif_app_sg.py
+++ streamlif_app_sg.py
@@ -48,8 +48,8 @@ streamlit.header("The fruit load list contains :")
 streamlit.dataframe(my_data_rows)
 
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add about?','jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
+add_my_fruit = streamlit.text_input('What fruit would you like to add ?','jackfruit')
+#streamlit.write('The user entered ', add_my_fruit)
 
 
 fruityvice_replay = requests.get("https://fruityvice.com/api/fruit/" + "jackfruit")
