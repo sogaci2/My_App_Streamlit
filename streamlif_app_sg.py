34c14a814314e1a38e286819353ba63bdab718d8
sogaci2 <113293174+sogaci2@users.noreply.github.com>
2022-09-12 10:31:52 +0200

    Update streamlif_app_sg.py

--- streamlif_app_sg.py
+++ streamlif_app_sg.py
@@ -32,19 +32,21 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')   #,'Kiwi') 
+   if not fuit_choice:
+        streamlit.error("Please select a fruit ot get information. ")
+   else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+        
 #streamlit.text(fruityvice_response.json())
+except URLEroor as e:
+    streamlit.error()
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
 
-#dont run anything past here while troubleshoot
+#don't run anything past here while troubleshoot
 
 streamlit.stop()
 
