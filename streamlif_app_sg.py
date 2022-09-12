eacbd33b3caddcc483acee0c68491b183eefe234
sogaci2 <113293174+sogaci2@users.noreply.github.com>
2022-09-12 11:08:34 +0200

    Update streamlif_app_sg.py

--- streamlif_app_sg.py
+++ streamlif_app_sg.py
@@ -30,6 +30,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
+def get_fruityvice_data(this_fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    #streamlit.dataframe(fruityvice_normalized)
+    return fruityvice_normalized
+
 streamlit.header("Fruityvice Fruit Advice!")
 
 try:
@@ -37,9 +43,8 @@ try:
    if not fruit_choice:
         streamlit.error("Please select a fruit ot get information. ")
    else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_function=get_fruityvice_date(fruit_choice)
+        streamlit.dataframe(back_from_function)
         
 #streamlit.text(fruityvice_response.json())
 except URLError as e:
